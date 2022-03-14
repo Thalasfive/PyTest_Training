d45ba2289b2a450aa49f01fd945d6552be6aeab5
Francois Muller <thalasfive@gmail.com>
2022-03-14 08:32:42 +0000

    Initial Commit

---
+++ Tests/sportscar/body/test_body.py
@@ -0,0 +1,18 @@
+import time
+
+from pytest import mark
+@mark.smoke
+@mark.body
+def test_body_functions_as_expected():
+    assert True
+@mark.bumper
+def test_bumper():
+    assert True
+@mark.windshield
+def test_windshield():
+    assert True
+
+@mark.ui
+def test_can_navigate_to_body_page(chrome_browser):
+    chrome_browser.get('https://www.motortrend.com/')
+    time.sleep(5)
---
+++ conftest.py
@@ -0,0 +1,11 @@
+import time
+from pytest import fixture
+from selenium import webdriver
+
+@fixture(scope='function')
+def chrome_browser():
+    browser = webdriver.Chrome()
+    yield browser
+
+    # Teardown
+    print('I am tearing down this browser')
